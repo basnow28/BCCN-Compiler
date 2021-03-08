73ee5889e0a9a7e27cd4796e0cc070217b16baee
Barbara Rekowska <basnow28@gmail.com>
2021-03-08 16:02:40 +0100

    Initial test

---
+++ test/lasalle/MainTest.java
@@ -0,0 +1,15 @@
+package lasalle;
+
+import org.junit.Test;
+import org.junit.platform.commons.annotation.Testable;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+
+    @Test
+    public void main() {
+        int i = 1;
+        assertEquals(i,1);
+    }
+}
